f33cb301176474febf6b33357b9ea3b7e846ea5c
Mihail Babaev <mmbabaev@gmail.com>
2017-09-20 19:07:52 +0300

    Initial Commit

---
+++ TinkoffChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinkoffChat
+//
+//  Created by Mihail Babaev on 20.09.17.
+//  Copyright © 2017 mbabaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
