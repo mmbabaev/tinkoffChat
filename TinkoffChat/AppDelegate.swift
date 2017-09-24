9045746f837efc357dfe69bb67fb038a41c87008
Mihail Babaev <mmbabaev@gmail.com>
2017-09-24 20:52:56 +0300

    add profile, app states printing

--- TinkoffChat/AppDelegate.swift
+++ TinkoffChat/AppDelegate.swift
@@ -8,39 +8,71 @@
 
 import UIKit
 
+enum AppState: String {
+    case notRunning, inactive, active, background, suspended
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    private var currentState = AppState.notRunning
+    
+    private func applicationMoved(to state: AppState) {
+        if (currentState == state && state == .background) {
+            currentState = .suspended
+            applicationMoved(to: .background)
+            
+            return
+        }
+        
+        print("Application moved from state \(currentState) to state \(state)\n")
+        currentState = state
+    }
+    
+    private func printCurrentState() {
+        print("Current state: \(currentState)\n")
+    }
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Function: \(#function))")
+        applicationMoved(to: .inactive)
+        
         return true
+    }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        print("Function: \(#function))")
+        printCurrentState()
+        
+        return true
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        print("Function: \(#function))")
+        applicationMoved(to: .active)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        print("Function: \(#function))")
+        applicationMoved(to: .inactive)
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("Function: \(#function))")
+        applicationMoved(to: .background)
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("Function: \(#function))")
+        applicationMoved(to: .inactive)
     }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // App move to "not running" only from "suspended"
+        currentState = .suspended
+        applicationMoved(to: .notRunning)
     }
-
-
 }
 
--- TinkoffChat/ViewController.swift
+++ TinkoffChat/ViewController.swift
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
